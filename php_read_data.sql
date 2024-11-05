60f7ad88d98d0ba8671b89a32245326d05b37d99
KidiXDev <dikhiardian55@gmail.com>
2024-11-05 21:44:01 +0700

    update SQL schema

--- php_read_data.sql
+++ php_read_data.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Waktu pembuatan: 22 Okt 2024 pada 11.55
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Host: localhost:3306
+-- Generation Time: Nov 05, 2024 at 02:35 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,31 +24,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `barang`
+-- Table structure for table `barang`
 --
 
 CREATE TABLE `barang` (
-  `id` int(10) NOT NULL,
-  `nama_barang` varchar(30) NOT NULL,
-  `jumlah` int(255) NOT NULL,
-  `harga` int(255) NOT NULL
+  `id` int NOT NULL,
+  `nama_barang` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `jumlah` int NOT NULL,
+  `harga` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `barang`
+-- Dumping data for table `barang`
 --
 
 INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
-(1, 'Laptop', 10, 15000000),
-(2, 'Kamera', 5, 8000000),
-(3, 'Smartphone', 20, 5000000),
-(4, 'Tablet', 15, 3000000),
-(5, 'Headphone', 30, 1000000),
-(6, 'Printer', 8, 2500000),
 (7, 'Keyboard', 25, 500000),
-(8, 'Mouse', 40, 300000),
-(9, 'Monitor', 12, 4000000),
-(10, 'Router', 18, 700000),
 (11, 'Flashdisk 16GB', 50, 200000),
 (12, 'Hard Disk 1TB', 25, 900000),
 (13, 'SSD 256GB', 30, 1200000),
@@ -56,7 +47,6 @@ INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
 (15, 'Speaker', 15, 800000),
 (16, 'Projector', 4, 20000000),
 (17, 'Power Bank 10000mAh', 40, 300000),
-(18, 'Charger Laptop', 35, 400000),
 (19, 'Smartwatch', 14, 2000000),
 (20, 'External DVD Drive', 10, 500000),
 (21, 'Graphics Card', 7, 6000000),
@@ -73,8 +63,7 @@ INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
 (32, 'Cable Organizer', 40, 100000),
 (33, 'Laptop Bag', 15, 300000),
 (34, 'HDMI Cable', 60, 100000),
-(35, 'VGA Cable', 50, 80000),
-(36, 'Ethernet Cable', 70, 150000),
+(35, 'VGA Cables', 50, 80000),
 (37, 'Surge Protector', 10, 400000),
 (38, 'Screen Protector', 20, 200000),
 (39, 'Wireless Adapter', 12, 500000),
@@ -82,7 +71,6 @@ INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
 (41, 'Tablet Stand', 14, 150000),
 (42, 'Portable Charger', 30, 350000),
 (43, 'Camera Tripod', 8, 600000),
-(44, 'Drone', 5, 2500000),
 (45, 'Game Controller', 20, 300000),
 (46, 'SD Card 64GB', 40, 250000),
 (47, 'Virtual Reality Headset', 3, 3000000),
@@ -104,7 +92,6 @@ INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
 (63, 'Studio Headphones', 10, 1200000),
 (64, 'Graphic Tablet', 8, 1800000),
 (65, 'Laptop Cooling Pad', 20, 400000),
-(66, 'Cable Management Box', 30, 250000),
 (67, 'WiFi Extender', 14, 600000),
 (68, 'Smart Light Bulb', 35, 150000),
 (69, 'Smart Plug', 28, 200000),
@@ -129,9 +116,7 @@ INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
 (88, 'Glue', 30, 50000),
 (89, 'Tape', 50, 20000),
 (90, 'Scissors', 45, 30000),
-(91, 'Ruler', 55, 15000),
 (92, 'Pencil Set', 60, 25000),
-(93, 'Pen Set', 70, 50000),
 (94, 'Notebook', 80, 100000),
 (95, 'Sticky Notes', 90, 20000),
 (96, 'Binder', 100, 50000),
@@ -142,27 +127,59 @@ INSERT INTO `barang` (`id`, `nama_barang`, `jumlah`, `harga`) VALUES
 (161, 'Corsair RM750', 8771, 2877000),
 (411, 'Zotac GeForce RTX 4090', 411, 23999999),
 (462, 'Samsung Evo 2TB', 3110, 2999999),
-(918, 'Asrock z790 Steel Legend Wifi', 773, 9111000);
+(918, 'Asrock z790 Steel Legend Wifi', 773, 9111000),
+(926, 'VGA Card High-End RTX 2080', 7322, 24999000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int NOT NULL,
+  `username` varchar(25) NOT NULL,
+  `password` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`) VALUES
+(1, 'Useradmin', 'ad173b6d7864f0dbcfcef93fb926cf66');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `barang`
+-- Indexes for table `barang`
 --
 ALTER TABLE `barang`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `barang`
+-- AUTO_INCREMENT for table `barang`
 --
 ALTER TABLE `barang`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=919;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=927;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
